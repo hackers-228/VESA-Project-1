8ab9d861ef88e1695c90ada999e25ba4e05d690d
hackers-228 <shivamburnwal567@gmail.com>
2026-08-07 22:04:37 +0530

    Create savings tracker program

--- savings-trackers.py
+++ savings-trackers.py
@@ -1,3 +1,9 @@
 print("===================================")
 print("     STUDENT SAVINGS TRACKER")
 print("===================================")
+
+goal = 500
+total_savings = 0
+
+print("Savings Goal: ₹", goal)
+print("Current Savings: ₹", total_savings)
