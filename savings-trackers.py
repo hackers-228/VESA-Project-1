4cbc7ec9511b73d300424cfdb50d6afd4e313f8e
hackers-228 <shivamburnwal567@gmail.com>
2026-08-07 22:05:58 +0530

    Add weekly savings loop

--- savings-trackers.py
+++ savings-trackers.py
@@ -4,6 +4,17 @@ print("===================================")
 
 goal = 500
 total_savings = 0
+week = 1
 
-print("Savings Goal: ₹", goal)
-print("Current Savings: ₹", total_savings)
+while total_savings < goal:
+    print("\nWeek", week)
+
+    amount = float(input("Enter savings: ₹"))
+
+    total_savings += amount
+
+    print("Current Savings: ₹", total_savings)
+
+    week += 1
+
+print("\nGoal Achieved!")
